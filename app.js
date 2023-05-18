f19a1ff94253fd578354c768ddec5de3852b66fc
aaa2002 <alexand2002@gmail.com>
2023-05-19 00:42:10 +0300

    Commit #3 - Handling CORS

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const {response} = require("express");
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false})); //false -> simpler url encoded bodies
+app.use(bodyParser.json());
+
+//Middle-ware for handling CORS (preventing them)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); //'*' = giving access to everyone
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); //TODO: read on these
+    if (req.method === 'OPTIONS') { //browsers always send options request first for get or put requests
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return response.status(200).json({});
+    }
+});
 
 //Routes handling requests
 app.use('/products', productRoutes);
