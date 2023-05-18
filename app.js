94cc3070caa6d16eae240b1a60112fdf7970a5cc
aaa2002 <alexand2002@gmail.com>
2023-05-19 00:43:51 +0300

    Commit #3 - Handling CORS

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return response.status(200).json({});
     }
+    next();
 });
 
 //Routes handling requests
